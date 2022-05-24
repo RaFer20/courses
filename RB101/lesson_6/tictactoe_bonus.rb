fb344c6134000d1875fcdbd42a3520c5dad3b418
Raul Fernandez <rafeye20@hotmail.com>
2022-05-24 00:37:13 -0500

    Updated tictactoe_bonus

---
+++ RB101/lesson_6/tictactoe_bonus.rb
@@ -0,0 +1,132 @@
+require 'pry'
+
+player_score = 0
+computer_score = 0
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You're the #{PLAYER_MARKER}. Computer is the #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+# rubocop:enable Metrics/AbcSize
+
+def display_score(player, computer)
+  puts "Your Score is:#{player}. Computer Score is:#{computer}"
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def joinor(arr, separator=', ', separator2='or ')
+  case arr.length
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{separator2} ")
+  else
+    arr[-1] = "#{separator2} #{arr.last}"
+    arr.join(separator)
+  end
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+    display_score(player_score, computer_score)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board) && detect_winner(board) == 'Player'
+    prompt "#{detect_winner(board)} scored a point!"
+    player_score += 1
+  elsif someone_won?(board) && detect_winner(board) == 'Computer'
+    prompt "#{detect_winner(board)} scored a point!"
+    computer_score += 1
+  else
+    prompt "It's a tie!"
+  end
+
+  next if player_score < 5 && computer_score < 5
+
+  prompt "Play again? (y/n)"
+  answer = gets.chomp
+  player_score = 0
+  computer_score = 0
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing Tictactoe, Goodbye"
